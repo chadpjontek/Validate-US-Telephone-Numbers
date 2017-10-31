2206c4183e242910ca612ab4a38c6e0578778227
Chad Pjontek <chadpjontek@gmail.com>
2017-10-31 01:48:46 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Validate-US-Telephone-Numbers
+Returns true if the passed string is a valid US phone number.
---
+++ Validate-US-Telephone-Numbers.js
@@ -0,0 +1,35 @@
+function telephoneCheck(str) {
+  // regex to validate number (2 options for with and without parentheses)
+  var re = /^[1]?[ ]?[0-9]{3}[-]?[ ]?[0-9]{3}[-]?[ ]?[0-9]{4}$|^[1]?[ ]?\([0-9]{3}\)[ ]?[0-9]{3}[-]?[ ]?[0-9]{4}$/;
+  if (str.match(re) === null) {
+    return false;
+  }
+  return true;
+}
+// Examples
+telephoneCheck("1 555-555-5555"); // returns true
+telephoneCheck("1 (555) 555-5555"); // returns true
+telephoneCheck("5555555555"); // returns true
+telephoneCheck("555-555-5555"); // returns true
+telephoneCheck("(555)555-5555"); // returns true
+telephoneCheck("1(555)555-5555"); // returns true
+telephoneCheck("555-5555"); // returns false
+telephoneCheck("5555555"); // returns false
+telephoneCheck("1 555)555-5555"); // returns false
+telephoneCheck("1 555 555 5555"); // returns true
+telephoneCheck("1 456 789 4444"); // returns true
+telephoneCheck("123**&!!asdf#"); // returns false
+telephoneCheck("55555555"); // returns false
+telephoneCheck("(6505552368)"); // returns false
+telephoneCheck("2 (757) 622-7382"); // returns false
+telephoneCheck("0 (757) 622-7382"); // returns false
+telephoneCheck("-1 (757) 622-7382"); // returns false
+telephoneCheck("2 757 622-7382"); // returns false
+telephoneCheck("10 (757) 622-7382"); // returns false
+telephoneCheck("27576227382"); // returns false
+telephoneCheck("(275)76227382"); // returns false
+telephoneCheck("2(757)6227382"); // returns false
+telephoneCheck("2(757)622-7382"); // returns false
+telephoneCheck("555)-555-5555"); // returns false
+telephoneCheck("(555-555-5555"); // returns false
+telephoneCheck("(555)5(55?)-5555"); // returns false
